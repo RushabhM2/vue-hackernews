4e271ef1e630e32dac12826c139974ce44cb45b9
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2021-05-04 15:55:05 +0300

    Chapter 2 complete

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,8 +1,9 @@
+import { shallowMount } from '@vue/test-utils'
 import Item from '../Item.vue'
-import Vue from 'vue'
 
 describe('Item.vue', () => {
-  test('sanity test', () => {
-    console.log(Item)
+  test('renders item', () => {
+    const wrapper = shallowMount(Item)
+    expect(wrapper.text()).toContain('item')
   })
 })
